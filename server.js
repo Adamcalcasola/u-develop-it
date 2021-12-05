6a2ce779fe8d62462fc0b3d196bc97b38ea70df9
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-05 15:11:23 -0500

    connect server to MySQL

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,11 +6,23 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'G02$leep4ever',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
