7ca6837a581d7444b13dd6819264dc4dcc14915c
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-12 23:10:07 -0500

    fixes

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'G02$leep4ever',
+        password: 'aaaaaa',
         database: 'election'
     },
     console.log('Connected to the election database.')
